86ab8a4d969355a743a88439344f636c7f021dfa
Ashin <ashin.c@sbsol.in>
2026-08-12 12:30:53 +0530

    Fix github pages blank screen (base path and HashRouter)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 import { ThemeProvider } from './context/ThemeContext.jsx'
@@ -8,12 +8,12 @@ import { ToolsProvider } from './context/ToolsContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ThemeProvider>
         <ToolsProvider>
           <App />
         </ToolsProvider>
       </ThemeProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
